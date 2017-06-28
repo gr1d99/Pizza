c77f447788904091e9f2e24c7959ae6f518fd04c
gr1d99 <gideonkimutai9@gmail.com>
2017-06-28 08:05:11 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+class Employee(object):
+    def __init__(self, name, salary=0):
+        self.name = name
+        self.salary = salary
+
+    def give_raise(self, percent):
+        new_salary = self.salary * percent
+        self.salary += new_salary
+        return 'New Salary is ', self.salary
+
+    def work(self):
+        print("%(name)s does stuff" % dict(name=self.name))
+
+    def __str__(self):
+        return "<Employee: %(name)s, Salary: %(salary)s>" % dict(name=self.name, salary=self.salary)
+
+    def __repr__(self):
+        return "<Employee: %(name)s, Salary: %(salary)s>" % dict(name=self.name, salary=self.salary)
